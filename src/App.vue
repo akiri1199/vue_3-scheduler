058d7dc0d0531fa050866243d01d45e57b449cdf
ThomasHook <vinusstar39@gmail.com>
2023-07-26 02:37:39 +0900

    change value

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
     <sc :setting="setting" :schedule-data="scData" @finish-Event="finishEvent"></sc>
+
   </div>
-  <div>
+
+  <div :style="{ width: '500px' }">
     <ul>
       <li v-for="(values, key) in state.items" :key="key">
         {{ key }}: {{ values.join(', ') }}
@@ -10,21 +12,16 @@
     </ul>
   </div>
 </template>
+
 <script>
 import schedulerLite from "./components/schedulerLite";
 import { reactive } from 'vue';
 const setting = {
-  unit: 24, //  it is division of hour set a among 1,2,3,4,5,6,8,9,10,12,15,16,18,20,24,30,32,36,40,45,48,60
+  unit: 15, //  it is division of hour set a among 1,2,3,4,5,6,8,9,10,12,15,16,18,20,24,30,32,36,40,45,48,60
   locale: 'es-ES' // en-GB  or es-ES
 };
 const sampleData = {   // insert saved Json data
-  Monday: ['01:30'],
-  ThuesDay: [],
-  Wednesday: ['00:00', '03:30'],
-  Thursday: ['02:30'],
-  Friday: [],
-  Saturday: [],
-  Sunday: []
+
 };
 
 export default {
@@ -47,7 +44,6 @@ export default {
     const state = reactive({
       items: []
     });
-
     return { state };
   }
 };
